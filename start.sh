3a5f1218726ae34f2c6a0d2e78f1319528b26327
the-onewho-knocks <borsehardik@gmail.com>
2026-04-21 12:16:16 +0530

    chNGES

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
 echo "Starting FastAPI server..."
-uvicorn backend.main:app --host 0.0.0.0 --port $PORT
+uvicorn backend.main:app --host 0.0.0.0 --port ${PORT:-8000}
